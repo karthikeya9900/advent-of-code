1fab21bfe9e929953a572e64f839acf29a6ae0b0
karthikeya <122973946+karthikeya9900@users.noreply.github.com>
2025-02-02 10:16:56 +0530

    2019 day1 completed

---
+++ 2019/day2/intCode.js
@@ -0,0 +1,54 @@
+const runIntCode = (code) => {
+  let index = 0;
+
+  while (index < code.length) {
+    if (code[index] === 99) {
+      return code;
+    }
+    if (code[index] === 1) {
+      code[code[index + 3]] = code[code[index + 1]] + code[code[index + 2]];
+    } else if (code[index] === 2) {
+      code[code[index + 3]] = code[code[index + 1]] * code[code[index + 2]];
+    }
+
+    index += 4;
+  }
+
+  return code;
+};
+
+// console.log(runIntCode(program));
+const nounAndVerb = () => {
+  let noun = 0;
+
+  while (noun < 100) {
+    if (100 * noun === 196907) {
+      return [noun, verb];
+    }
+    noun++;
+  }
+};
+
+console.log(nounAndVerb());
+
+// console.log(program.length);
+// console.log(index, "current index");
+// console.log(code[index], "index of data", code[code[index]], "data");
+// console.log(
+//   code[index + 1],
+//   "index of data",
+//   code[code[index + 1]],
+//   "data"
+// );
+// console.log(
+//   code[index + 2],
+//   "index of data",
+//   code[code[index + 2]],
+//   "data"
+// );
+// console.log(
+//   code[index + 3],
+//   "index of data",
+//   code[code[index + 3]],
+//   "data"
+// );
